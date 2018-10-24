65cf884a771b86e2a645a10b6cb148f382d22c14
BitNetwork (Lawrence Tech) <lawrenceoffline@gmail.com>
2018-10-24 12:14:51 +0100

    Update welcome.php

--- adminlogin/welcome.php
+++ adminlogin/welcome.php
@@ -23,9 +23,7 @@ if(!isset($_SESSION['username']) || empty($_SESSION['username'])){
     <div class="page-header">
         <h1>Hi, <b><?php echo htmlspecialchars($_SESSION['username']); ?></b>. Welcome.</h1>
     </div>
-    <center><iframe src="https://winsford.showmyhomework.co.uk/school/homeworks/calendar?year=Year%2011" style="width: 100% ; height:850px ; border:0;"></iframe></center>
-    <center><iframe src="https://drive.google.com/embeddedfolderview?id=1QS1ee6qYZScmbIpGj21pV3Uuox_6q_mG#list" style="width:100%; height:600px; border:0;"></iframe></center>
-
+    <p>Admin logged in.</p>
     <p><a href="logout.php" class="btn btn-danger">Sign Out of Your Account</a></p>
 </body>
 </html>
